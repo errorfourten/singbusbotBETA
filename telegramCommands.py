ca8661c4d845ce3dafe138d1b418f38f2d89496c
errorfourten <yihhang@gmail.com>
2018-02-03 12:57:56 +0800

    Initial commit

---
+++ telegramCommands.py
@@ -0,0 +1,15 @@
+from singbusbot import send_message_to_owner
+
+def check_commands(bot, update, message):
+    message = message.split(" ")
+    if message[0] == "/help":
+        return "<b>Help Menu</b>\n/help - For a list of commands\n/about - About page\n/feedback - Send feedback straight to the developer by using this format - /feedback [text]"
+    elif message[0] == "/about":
+        return "<b>About Page</b>\nThank you for using SingBusBot. \nPlease do check out the code at https://github.com/errorfourten/singbusbot and new ideas are always welcome."
+    elif message[0] == "/start":
+        return "Welcome to Singapore Bus Bot! Just enter a bus stop code and find the next bus. For example, try entering 53009 or Bishan Int.\nDon't know what your bus stop code is? Take a look at the top right of the information boards or check https://www.transitlink.com.sg/eservice/eguide/service_idx.php with your bus number to look up the bus stop code. \n\n /help for more information"
+    elif message[0] == "/feedback":
+        send_message_to_owner(bot, " ".join(message[1:]))
+        return "Thank you for your feedback! \"{}\"".format(" ".join(message[1:]))
+    else:
+        return False
